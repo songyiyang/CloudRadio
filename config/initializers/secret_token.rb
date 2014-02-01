28291bc9f8ed1572b70cf14ae40991f7802026a9
Yiyang Song <songyiyang2012@gmail.com>
2014-02-01 14:20:09 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CloudRadio::Application.config.secret_key_base = 'b3c8eeb778b849aed1b10896cd74a926707a32efc50010acc058d7176e37d2a4c824f826eeb61024823e6fd89de2869832b035c3c589603759ed1b9c2035920e'
